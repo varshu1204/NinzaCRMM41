6a1adeb456ed1c649622cdd9d880e728434c6443
QSP <QSP@DESKTOP-K5OA249>
2025-12-04 10:38:56 +0530

    Commited

---
+++ src/test/java/Demo1/DemoTest.java
@@ -0,0 +1,12 @@
+package Demo1;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test(groups = "smoke")
+	public void Dhoni()
+	{
+		System.out.println("dhoni");
+	}
+}
---
+++ src/test/java/Demo1/SampleTest.java
@@ -0,0 +1,12 @@
+package Demo1;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test(groups = "regression")
+	public void Virat()
+	{
+		System.out.println("Virat");
+	}
+}
